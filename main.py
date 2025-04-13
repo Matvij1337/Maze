66a81a17744aa58ba88516239317d714fabe0be5
Matvij1337 <matvijkolinko068@gmail.com>
2025-04-13 18:02:45 +0300

    new update V1.1

--- main.py
+++ main.py
@@ -8,10 +8,29 @@ DISPLAY_SIZE = (700, 500)
 
 FPS = 60
 
+mixer.init()
+mixer.music.load("CAMOrOH.ogg")
+mixer.music.play()
+
 window = display.set_mode(DISPLAY_SIZE)
 display.set_caption("Maze")
+
+class Gamespite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+     window.blit(self.image, (self.rect.x, self.rect.y))
+
 background = transform.scale(image.load("backgroundmaze.jpg"), DISPLAY_SIZE)
-#sprite1 = transform.scale(image.load("ukraine.png"))
+player = Gamespite(("ukraine.png"), 100, 350, 3)
+monster = Gamespite(("russia.png"), 500, 70, 2)
+final = Gamespite(("NATO.png"), DISPLAY_SIZE[0] - 250, DISPLAY_SIZE[1] - 200, 0)
 
 
 game = True
@@ -20,7 +39,8 @@ while game:
         if e.type == QUIT:
                 game = False
     window.blit(background, (0, 0))
-
-
+    player.reset()
+    monster.reset()
+    final.reset()
     display.update()
     clock.tick(FPS)
