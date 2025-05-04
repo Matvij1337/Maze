6483a4d9d2972a6eb781904581261599370998d7
Matvij1337 <matvijkolinko068@gmail.com>
2025-05-04 17:50:01 +0300

    New update! V1.4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
+import time as t
+
 
 from pygame import *
 init()
@@ -9,8 +10,10 @@ DISPLAY_SIZE = (700, 500)
 FPS = 60
 
 mixer.init()
-mixer.music.load("CAMOrOH.ogg")
-mixer.music.play()
+#mixer.music.load("CAMOrOH.ogg")
+#mixer.music.play()
+
+slap = mixer.Sound("slap.ogg")
 
 window = display.set_mode(DISPLAY_SIZE)
 display.set_caption("Maze")
@@ -18,7 +21,7 @@ display.set_caption("Maze")
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (45, 45))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -43,7 +46,7 @@ class Enemy(GameSprite):
     direction = "left"
 
     def update(self):
-        if self.rect.x < DISPLAY_SIZE[0] - 500:
+        if self.rect.x < DISPLAY_SIZE[0] - 550:
             self.direction = "right"
         if self.rect.x > DISPLAY_SIZE[0] - 65:
             self.direction = "left"
@@ -79,35 +82,65 @@ walls.append(Wall(240, 100, 150, 70, (144, 198, 124)))
 walls.append(Wall(490, 100, 20, 90, (144, 198, 124)))
 walls.append(Wall(490, 100, 100, 20, (144, 198, 124)))
 walls.append(Wall(580, 100, 20, 100, (144, 198, 124)))
-walls.append(Wall(510, 180, 70, 20, (185, 128, 164)))
+secret_wall = Wall(510, 180, 70, 20, (144, 198, 124))
 walls.append(Wall(220, 280, 450, 20, (144, 198, 124)))
 walls.append(Wall(220, 370, 380, 20, (144, 198, 124)))
 walls.append(Wall(530, 370, 20, 110, (144, 198, 124)))
-#walls.append(Wall(, , , , (144, 198, 124)))
+
 
 background = transform.scale(image.load("backgroundmaze.jpg"), DISPLAY_SIZE)
-player = Player("ukraine.png", 100, 350, 3)
-monster = Enemy(("russia.png"), 500, 70, 2)
-final = GameSprite(("NATO.png"), DISPLAY_SIZE[0] - 250, DISPLAY_SIZE[1] - 200, 0)
+player = Player("ukraine.png", 50, 400, 3)
+monster = Enemy(("russia.png"), 450, 50, 2)
+secret = GameSprite(("NATO.png"), DISPLAY_SIZE[0] - 175, DISPLAY_SIZE[1] - 375, 0)
+final = GameSprite(("EU_union.png"), DISPLAY_SIZE[0] - 150, DISPLAY_SIZE[1] - 100, 0)
 
-
+score_point = 0
+finish = False
 game = True
 while game:
     for e in event.get():
         if e.type == QUIT:
                 game = False
+    if not finish:
+        for wall in walls:
+            if sprite.collide_rect(player, wall):
+                slap.play()
+                player.rect.x = 50
+                player.rect.y = 400
+                score_point += 1
 
+        if sprite.collide_rect(player, monster):
+            slap.play()
+            player.rect.x = 50
+            player.rect.y = 400
+            score_point += 1
 
+        window.blit(background, (0, 0))
+        player.reset()
+        player.update()
+        monster.reset()
+        monster.update()
+        secret.reset()
+        final.reset()
 
-    window.blit(background, (0, 0))
-    player.reset()
-    player.update()
-    monster.reset()
-    monster.update()
-    final.reset()
+        secret_wall.draw()
 
-    for wall in walls:
-        wall.draw()
+        for wall in walls:
+            wall.draw()
+
+        if score_point == 3:
+            break
+
+        if sprite.collide_rect(player, final):
+            mixer.music.load("Hymn_Ukraine.ogg")
+            mixer.music.play()
+            finish = True
+
+        if sprite.collide_rect(player, secret):
+            mixer.music.load("Hymn_Ukraine.ogg")
+            mixer.music.play()
+            finish = True
 
     display.update()
     clock.tick(FPS)
+
